27d528bd527ff24d69ca1c1297e721207b41a856
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-12-19 14:30:19 +0000

    cli example of inserting markdown in our Vue App

---
+++ marked-example/README.md
@@ -0,0 +1,28 @@
+# marked-example
+
+See 
+
+* [Parsing Markdown in Vue with the Marked Library](https://youtu.be/lXc0c1n6O-g)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ marked-example/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Learning about Markdown inside Vue"/>
+    {{ markdown }}
+    <div v-html="markdown2Html"></div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import { marked } from 'marked';
+
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      markdown: 
+      `# Hello World\n\n**bold**\n*italics*\n## Subsection`
+    }
+  },
+  computed: {
+    markdown2Html() {
+      return marked(this.markdown)
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
